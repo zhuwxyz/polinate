d85f17711c30c258ef65b3330e698d11466959c0
Willie Zhu <zhuwillie11@gmail.com>
2015-11-14 17:06:21 -0500

    Add speech model

--- client/polinate.js
+++ client/polinate.js
@@ -1,6 +1,11 @@
 Speeches = new Mongo.Collection("speeches");
 
 Template.Index.helpers({
+  link: function () {
+    console.log(this);
+    return "/speech/" + this._id;
+  },
+
   speech: function () {
     return Speeches.find({}).fetch();
   }
