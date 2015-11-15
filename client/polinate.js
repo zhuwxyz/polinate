99fa2f345d1ed9875f4bc6f89a40aa3816c81060
jsegaran <jsegaran1@gmail.com>
2015-11-14 23:05:15 -0500

    UI is coming together

--- client/polinate.js
+++ client/polinate.js
@@ -1,5 +1,5 @@
 Speeches = new Mongo.Collection("speeches");
-
+Prescan  = new Mongo.Collection("prescan");
 Template.Index.helpers({
   link: function () {
     return "/speech/" + this._id;
@@ -7,5 +7,22 @@ Template.Index.helpers({
 
   speech: function () {
     return Speeches.find({}).fetch();
+  }
+});
+Template.prescan.helpers({
+  prescan: function() {
+    return Prescan.find({}).fetch();
+  },
+  pic: function() {
+    return "/" + this.picname;
+  },
+  Name: function() {
+    return this.name;
+  },
+  Party: function() {
+    return this.Party
+  },
+  Partypic: function(){
+    return "/" + this._Party;
   }
 });
--- server/polinate.js
+++ server/polinate.js
@@ -1,4 +1,5 @@
 Speeches = new Mongo.Collection("speeches");
+Prescan  = new Mongo.Collection("prescan");
 
 Meteor.startup(function () {
   // code to run on server at startup
